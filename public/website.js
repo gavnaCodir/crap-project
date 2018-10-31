1e6b2a443317d646861a6184507e474e8551302a
mark batin <markbatin2017@gmail.com>
2018-10-31 19:25:24 +0400

    very bad slider

---
+++ public/website.js
@@ -0,0 +1,20 @@
+var arr = [6,4,8,2,10,14,12]
+
+
+var arrSort = (array) => {
+
+
+for(let i= 0;i<arr.length;i++){
+	var x = arr[i];
+	var y = arr[0]
+	console.log(x)
+    if(x>x+1){
+    	
+    	console.log(x)
+    }
+}
+
+
+}
+
+arrSort(arr)
