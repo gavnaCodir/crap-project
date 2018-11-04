a0e1cea65b54d5d69ccbbd55e4225dc72213e104
mark batin <markbatin2017@gmail.com>
2018-11-04 16:10:28 +0400

    contact form + smooth scrolling

--- localhost.js
+++ localhost.js
@@ -1,9 +1,69 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer');
 const port = 8080;
 
-
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+
+app.post('/',urlencodedParser,(req,res)=>{
+	var emailOutput=`
+	<h2>New data from new costummer</h2>
+	<ol>
+		<li>Name: ${req.body.Name}</li>
+		<li>Email: ${req.body.Email}</li>
+		<li>Phone number: ${req.body.Phone}</li>
+	</ol>
+	<h2>Message:</h2>
+	<p>${req.body.Message}</p>
+	`;
+	console.log(req.body);
+
+
+	 let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: 'testnodemailermark@gmail.com', r
+            pass: 'asdfghjkl_12345' 
+        },
+        tls:{
+        	rejectUnauthorized:false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Best website" <testnodemailermark@gmail.com', // sender address
+        to: 'testnodemailermark@gmail.com', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world?', // plain text body
+        html: emailOutput // html body
+    };
+
+    //     let mailOptions2 = {
+    //     from: '"Best website" <testnodemailermark@gmail.com', // sender address
+    //     to: `${req.body.Email}`, // list of receivers
+    //     subject: 'Hello ✔', // Subject line
+    //     text: 'Hello world?', // plain text body
+    //     html: emailOutput // html body
+    // };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        res.render('website',{data:'Email has been sent'})
+    });
+});
+
 
 app.get('/',(req,res)=>{
 	res.sendFile(__dirname +'/public/website.html')
--- public/website.js
+++ public/website.js
@@ -1,8 +1,15 @@
-function q(element){return  document.querySelector(element)}
-let leftArr = q('.arrowSlide1');
-let rightArr = q('.arrowSlide2');
-let slider = q('.slider');
-let bullets = q('.bullets');
+function q(element){return  document.querySelector(element)};
+let leftArr = q('.arrowSlide1'),
+rightArr = q('.arrowSlide2'),
+slider = q('.slider'),
+bullets = q('.bullets'),
+navHome = q('.navHome'),
+navAbout = q('.navAbout'),
+navServ = q('.navServ'),
+navTeam = q('.navTeam'),
+navPrice = q('.navPrice');
+
+
 
 leftArr.addEventListener('click',()=>slideFun(minus));
 rightArr.addEventListener('click',()=>slideFun(plus));
@@ -17,18 +24,21 @@ page>4?page=0:page<0?page=4:page;
 let l = page * -100;
 slider.style.left = l + '%';
 
-// if(page===0){
-// 	bullets[0].className="bb"
-// 	console.log(bullets[0].className)
-// }else if(page===1){
-	
-// }
+};
 
+
+navHome.addEventListener('click',()=>scrollFun(0));
+navAbout.addEventListener('click',()=>scrollFun(1));
+navServ.addEventListener('click',()=>scrollFun(3.67));
+navTeam.addEventListener('click',()=>scrollFun(4.67));
+navPrice.addEventListener('click',()=>scrollFun(5.77))
+
+var scrollFun = (num)=>{
+window.scrollTo({
+    top: window.innerHeight*num,
+    behavior: "smooth"
+});
 }
-
-
-
-
 
 
 // var arr = [6,4,8,2,10,14,12]
